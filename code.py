5ae80ffbe9b572cda9e25436b73aa25c68d03227
Eric Skiff <eric@tanookilabs.com>
2022-10-19 14:57:47 -0400

    various status lights

--- code.py
+++ code.py
@@ -11,8 +11,11 @@ import wifi
 import socketpool
 import adafruit_requests
 import rtc
-import os 
+import os
+import neopixel 
 from adafruit_lc709203f import LC709203F
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 # Get wifi details and more from a secrets.py file
 try:
@@ -23,13 +26,14 @@ except ImportError:
 
 ## URL
 HEATSEEK_URL = "http://relay.heatseek.org/temperatures"
-CODE_VERSION = "F-ESP-1.3.1"
+CODE_VERSION = "F-ESP-1.3.2"
 VOLT_DIFF_FOR_CHARGE = 0.03
 QUIET_MODE_SLEEP_LENGTH = 600
 
 ## FUNCTIONS
 
 def deep_sleep(secs):
+    fade_status(0, 0, 128, 3, 1)
     # go to sleep for an hour and see if it's time to wake up from quiet mode next time
     time_to_wake = time.monotonic() + secs
     # set the time alarm, notice that monotonic_time here is a named argument and must be set in the function call
@@ -51,6 +55,7 @@ def handle_quiet_mode(new_voltage):
         f.close()
         print('QUIET MODE DATA - new voltage:{}, last_voltage:{}'.format(new_voltage, last_voltage))
         if(new_voltage > float(last_voltage) + VOLT_DIFF_FOR_CHARGE):
+            fade_up_status(50, 0, 128, 3, 2)
             print("QUIET MODE ENDED - battery is charging. Unit must be plugged in")
             print("Clearing battery.txt and quiet.txt")
             # Higher voltage even with some offset! We're plugged in
@@ -66,18 +71,15 @@ def handle_quiet_mode(new_voltage):
                 print("QUIET MODE: overwriting battery.txt low water mark")
                 with open('battery.txt', "w") as bf:
                     bf.write('{}\n'.format(new_voltage))
-            # Either way, go to sleep for an hour and see if it's time to wake up from quiet mode next time
-            time_to_wake = time.monotonic() + QUIET_MODE_SLEEP_LENGTH
-            # set the time alarm, notice that monotonic_time here is a named argument and must be set in the function call
-            time_alarm = alarm.time.TimeAlarm(monotonic_time=time_to_wake)
-            # Exit the program, and then deep sleep until the alarm wakes us.
-            print("QUIET MODE: Sleeping for 1 hr")
-            alarm.exit_and_deep_sleep_until_alarms(time_alarm)
+            fade_status(50, 0, 128, 3, 2)
+            print("QUIET MODE: Sleeping for "+ str(QUIET_MODE_SLEEP_LENGTH / 60) + " min")
+            deep_sleep(QUIET_MODE_SLEEP_LENGTH)
     else:
         print("QUIET MODE: writing new battery.txt")
         with open('battery.txt', "w") as bf:
             bf.write('{}\n'.format(new_voltage))
-            print("QUIET MODE: Sleeping for 1 hr")
+            fade_status(50, 0, 128, 3, 2)
+            print("QUIET MODE: Sleeping for "+ str(QUIET_MODE_SLEEP_LENGTH / 60) + " min")
             deep_sleep(QUIET_MODE_SLEEP_LENGTH)
             
 ## END OF FUNCTION handle_quiet_mode
@@ -124,6 +126,36 @@ def transmit_queue(requests):
             return False
 ## END OF FUNCTION - transmit_queue(requests)
 
+def flash_status(red=128, green=128, blue=128, flash_length=0.5, repeat=1):
+    for x in range(0, repeat):
+        pixels.fill((red, green, blue))
+        time.sleep(flash_length)
+        pixels.fill((0, 0, 0))
+        time.sleep(flash_length)
+
+def flash_warning(red=128, green=0, blue=0, red2=128, green2=128, blue2=0,flash_length=0.5, repeat=4):
+    for x in range(0, repeat):
+        pixels.fill((red, green, blue))
+        time.sleep(flash_length)
+        pixels.fill((red2, green2, blue2))
+        time.sleep(flash_length)
+    pixels.fill((0, 0, 0))
+
+def fade_status(red=0, green=0, blue=128, fade_length=3, repeat=1):
+    for x in range(0, repeat):
+        for y in range(1, fade_length * 100):
+            pixels.fill((int(red / y), int(green / y), int(blue / y)))
+            time.sleep(fade_length / 100)
+    pixels.fill((0, 0, 0))    
+
+
+def fade_up_status(red=128, green=128, blue=128, fade_length=3, repeat=1):
+    for x in range(0, repeat):
+        for y in range(fade_length * 100, 1):
+            pixels.fill((int(red / y), int(green / y), int(blue / y)))
+            time.sleep(fade_length / 100)
+    pixels.fill((0, 0, 0))  
+
 ## MAIN CODE BLOCK
 
 led = digitalio.DigitalInOut(board.LED)
@@ -141,7 +173,7 @@ led.direction = digitalio.Direction.OUTPUT
 print("Starting up, blink slow then fast for 6 sec")
 for x in range(8):
     led.value = not led.value
-    time.sleep(0.25)
+    time.sleep(0.5)
 for x in range(8):
     led.value = not led.value
     time.sleep(0.5)
@@ -165,10 +197,11 @@ try:
     sensor = adafruit_ahtx0.AHTx0(i2c)
     # If the sensor is connected, go to read only mode so we can write temperatures
     print("\nSENSOR DETECTED, attempting to writing to temperatures.txt, CIRCUITPY is read-only by computer")
+    flash_status(0,128,0,0.5,1)
     # storage.remount("", switch.value)
 except ValueError:
     print("\nNO SENSOR, not writing to temperatures.txt CIRCUITPY is writeable by computer")
-
+    flash_status(0,0,128,0.5,1)
 
 reading_interval = int(secrets["reading_interval"])
 net_connected = False
@@ -187,16 +220,17 @@ try:
         pool = socketpool.SocketPool(wifi.radio)
         requests = adafruit_requests.Session(pool, ssl.create_default_context())
         net_connected = True
+        flash_status(0,128,0,1,2)
     except: 
         print("Connecting to fallback network %s"%secrets["heatseek_wifi_ssid"])
         wifi.radio.connect(secrets["heatseek_wifi_ssid"], secrets["heatseek_wifi_password"])
         print("Connected to %s!"%secrets["heatseek_wifi_ssid"])
         print("My IP address is", wifi.radio.ipv4_address)
-
         ## Set up http request objects
         pool = socketpool.SocketPool(wifi.radio)
         requests = adafruit_requests.Session(pool, ssl.create_default_context())
         net_connected = True
+        flash_status(0,128,0,1,2)
         
     ## Set the time if this is a cold boot
     if not alarm.wake_alarm:
@@ -213,12 +247,15 @@ try:
         print(f"System Time: {r.datetime}")
 except ConnectionError:
     print("Could not connect to network.")
+    flash_warning()
     net_connected = False
 except ValueError:
     print("Time response was invalid (no connection or bad data)")
+    flash_warning()
     net_connected = False
 except:
     print("An error occured connecting to the network or time server")
+    flash_warning()
     net_connected = False
 
 # ensure the time matches the RTC's time
@@ -257,6 +294,7 @@ try:
         if response.status_code == 200:
             print("SUCCESS sending to Heat Seek at {}".format(time.time()))
             send_success = True
+            flash_status(128,128,128, 0.5, 3)
             transmit_queue(requests)
         else:
             print("Sending heatseek data failed")
@@ -266,6 +304,7 @@ try:
         if 'queue' not in os.listdir():
             os.mkdir('queue')
         with open("/queue/{}.txt".format(time.time()), "a") as qp:
+            fade_status(128, 128, 0, 2, 2)
             print("Couldn't send, writing a queue file")
             print('{},{},{}'.format(time.time(), ((sensor.temperature * 1.8) + 32), sensor.relative_humidity))
             qp.write('{},{},{}\n'.format(time.time(), ((sensor.temperature * 1.8) + 32), sensor.relative_humidity))
@@ -278,6 +317,7 @@ try:
 except OSError as e:  # Typically when the filesystem isn't writeable...
     if e.args[0] == 28:  # If the file system is full...
         print("\nERROR: filesystem full\n")
+    flash_warning()
     print("\nWARN: not writing temp to file, or sending to Heat Seek")
     print("This is  likely because sensor is not attached and the filesystem was writable by USB")
     print("If this is unexpected, be sure you reset the feather after plugging in the sensor to run boot.py again.")
